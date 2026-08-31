425297ae1342d95b3097a6223411118ce644126b
Utas Prasojo <utasprasojo229@gmail.com>
2026-08-31 14:24:48 +0700

    init

---
+++ src/Mahasiswa/Mahasiswa.java
@@ -0,0 +1,66 @@
+package Mahasiswa;
+
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String jurusan;
+    private double ipk;
+
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String cekKelulusan() {
+        if (ipk >= 3.00) {
+            return "Lulus";
+        } else {
+            return "Belum Lulus";
+        }
+    }
+
+    public void updateIpk(double ipkBaru) {
+        this.ipk = ipkBaru;
+    }
+
+    public String hitungPredikat() {
+        if (ipk >= 3.75) {
+            return "Dengan Pujian";
+        } else if (ipk >= 3.50) {
+            return "Sangat Memuaskan";
+        } else if (ipk >= 3.00) {
+            return "Memuaskan";
+        } else {
+            return "Perlu Perbaikan";
+        }
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Status: " + cekKelulusan());
+        System.out.println("Predikat: " + hitungPredikat());
+        System.out.println();
+    }
+}
